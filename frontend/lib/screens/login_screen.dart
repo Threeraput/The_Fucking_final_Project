1aa2c9f74738f905166584cba5052944d7b411ea
Threeraput <threeraput5tmr@gmail.com>
2025-11-05 21:45:44 +0700

    v3

--- frontend/lib/screens/login_screen.dart
+++ frontend/lib/screens/login_screen.dart
@@ -153,7 +153,7 @@ class _LoginScreenState extends State<LoginScreen> {
                     
                     TextField(
                       controller: _passwordController,
-                      keyboardType: TextInputType.number,                 
+                                   
                       decoration: InputDecoration(
                         prefixIcon: const Icon(Icons.lock_outline),
                         labelText: 'Password',
